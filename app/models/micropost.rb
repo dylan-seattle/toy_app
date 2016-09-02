81395a575e7f81425e8d0dc403029a7267cbba2f
Dylan N <abathur.zerglinger@gmail.com>
2016-09-02 00:50:24 +0000

    Finish up Toy App

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,2 +1,7 @@
 class Micropost < ApplicationRecord
+  #microposts belongs to user
+  belongs_to :user
+  #validates max length
+  validates :content, length: { maximum: 140 }, presence: true
+
 end
